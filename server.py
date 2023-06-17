3401100d12b6147fc7f2ba459a6f6fddd6a73324
Decraene Robin <robin.decraene@student.arteveldehs.be>
2023-06-17 16:42:33 +0200

    html bestanden

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import snake
+
 
 HOST = "192.168.50.195"
 PORT = 9999
@@ -11,15 +10,8 @@ class NeuralHTTP(BaseHTTPRequestHandler):
     self.send_header("Content-type", "text/html")
     self.end_headers()
 
-self.wfile.write(bytes("<html><body><h1>hello</h1></body></html>", "utf-8"))
+    self.wfile.write(bytes("<html><body><h1>hello</h1></body></html>", "utf-8"))
 
-# def do_POST(self):
-#   self.send_response(200)
-#   self.send_header("Content-type", "text/html")
-#   self.end_headers()
-
-# if self.path == "/start_game":
-#   snake.start_game()
 
 server = HTTPServer((HOST, PORT), NeuralHTTP)
 print("Server now running...")
