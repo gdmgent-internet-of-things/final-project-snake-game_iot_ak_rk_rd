f442986f3d647775886a0297e1f4e36245a8900c
Decraene Robin <robin.decraene@student.arteveldehs.be>
2023-06-20 13:49:51 +0200

    snake rotate

--- flask-app/snake3.py
+++ flask-app/snake3.py
@@ -30,15 +30,11 @@ class SNAKE:
         self.direction = Vector2(1, 0)
         self.new_block = False
 
-        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
-        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
-        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
-        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+        self.head = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_rotation = 0
 
-        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
-        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
-        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
-        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+        self.tail = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_rotation = 0
 
         self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
         self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
@@ -52,64 +48,71 @@ class SNAKE:
         self.update_head_graphics()
         self.update_tail_graphics()
 
-        for index,block in enumerate(self.body):
-             # 1. A rect for the poistioning
-             x_pos = int(block.x * cell_size)
-             y_pos = int(block.y * cell_size)
-             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+        for index, block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
 
-             # 2. what direction is the face heading
-             if index == 0:
-                   screen.blit(self.head,block_rect)
-             elif index == len(self.body) - 1:
-                  screen.blit(self.tail,block_rect)
-             else: 
-                 previous_block = self.body[index + 1] - block
-                 next_block = self.body[index - 1] - block
-                 if previous_block.x == next_block.x:
-                    screen.blit(self.body_vertical,block_rect)
-                 elif previous_block.y == next_block.y:
-                    screen.blit(self.body_horizontal,block_rect)
-                 else:
+            if index == 0:
+                rotated_head = pygame.transform.rotate(self.head, self.head_rotation)
+                screen.blit(rotated_head, block_rect)
+            elif index == len(self.body) - 1:
+                rotated_tail = pygame.transform.rotate(self.tail, self.tail_rotation)
+                screen.blit( rotated_tail, block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
                     if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
-                     screen.blit(self.body_tl,block_rect)
+                        screen.blit(self.body_tl, block_rect)
                     elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
-                     screen.blit(self.body_bl,block_rect)
+                        screen.blit(self.body_bl, block_rect)
                     elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
-                     screen.blit(self.body_tr,block_rect)
+                        screen.blit(self.body_tr, block_rect)
                     elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
-                     screen.blit(self.body_br,block_rect)
-                                          
-        
+                        screen.blit(self.body_br, block_rect)
+
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
-        if head_relation == Vector2(1,0): self.head = self.head_left
-        elif head_relation == Vector2(-1,0): self.head = self.head_right
-        elif head_relation == Vector2(0,1): self.head = self.head_up
-        elif head_relation == Vector2(0,-1): self.head = self.head_down
+        if head_relation == Vector2(1, 0):
+            self.head_rotation = 90
+        elif head_relation == Vector2(-1, 0):
+            self.head_rotation = 270
+        elif head_relation == Vector2(0, 1):
+            self.head_rotation = 0
+        elif head_relation == Vector2(0, -1):
+            self.head_rotation = 180
 
     def update_tail_graphics(self):
         tail_relation = self.body[-2] - self.body[-1]
-        if tail_relation == Vector2(1,0): self.tail = self.tail_left
-        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
-        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
-        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
-            
-      #error with snake tail 
+        if tail_relation == Vector2(1, 0):
+            self.tail_rotation = 90
+        elif tail_relation == Vector2(-1, 0):
+            self.tail_rotation = 270
+        elif tail_relation == Vector2(0, 1):
+            self.tail_rotation = 0
+        elif tail_relation == Vector2(0, -1):
+            self.tail_rotation = 180
+
 
     def move_snake(self):
-      if self.new_block == True: 
-          body_copy = self.body[:]
-          body_copy.insert(0,body_copy[0] + self.direction)
-          self.body = body_copy[:]
-          self.new_block = False
-      else:
-          body_copy = self.body[:-1]
-          body_copy.insert(0,body_copy[0] + self.direction)
-          self.body = body_copy[:]
+        if self.new_block:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
 
     def add_block(self):
-       self.new_block = True
+        self.new_block = True
+
 
 class DOT:
     def __init__(self):
